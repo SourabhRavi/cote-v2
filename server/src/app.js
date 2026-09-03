56beb4e861e89383dd5e1da8f262575f9fdb213e
SourabhRavi <sourabh.k.ravi@gmail.com>
2026-09-03 13:39:03 +0530

    feat: implement CORS middleware for API and update endpoint paths

--- server/src/app.js
+++ server/src/app.js
@@ -5,21 +5,22 @@ import workspaceRouter from "./modules/workspace/workspace.route.js";
 import channelRouter from "./modules/channel/channel.route.js";
 import messageRouter from "./modules/message/message.route.js";
 import { requireAuth } from "./middleware/auth.middleware.js";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use((req, res, next) => {
-//   console.log("Req received");
-//   next();
-// });
-
-app.get("/me", requireAuth, (req, res) => {
+app.use("/api/v1/auth", authRouter);
+app.get("/api/v1/me", requireAuth, (req, res) => {
   res.json(req.user);
 });
-
-app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/workspaces", requireAuth, workspaceRouter);
 app.use("/api/v1/channels", requireAuth, channelRouter);
 app.use("/api/v1/messages", requireAuth, messageRouter);
