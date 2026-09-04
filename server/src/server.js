ffee215247f38ea8bb695ea93ec48b609e5315b2
SourabhRavi <sourabh.k.ravi@gmail.com>
2026-09-04 15:46:24 +0530

    refactor: remove socket.io-client dependency and related authentication code
    - Removed socket.io-client from package.json as it is no longer needed.
    - Eliminated socket authentication middleware from server.js.
    - Deleted socket-test.js as it contained tests for the removed functionality.

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ import "dotenv/config";
 import app from "./app.js";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
-import { socketAuth } from "./socket/socket.auth.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,11 +9,8 @@ const server = createServer(app);
 
 const io = new Server(server);
 
-io.use(socketAuth);
-
 io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
-  console.log("Authenticated user:", socket.user.id);
+  console.log("Client connected", socket.id);
 });
 
 server.listen(PORT, () => {
