c62776cefea4b790b4b7ef27283b6113466dff17
SourabhRavi <sourabh.k.ravi@gmail.com>
2026-09-05 16:22:41 +0530

    fix(socket): correct MESSAGE_DELETE event to use MESSAGE_UPDATE identifier

--- client/src/lib/socket-events.ts
+++ client/src/lib/socket-events.ts
@@ -1,6 +1,10 @@
 export const SOCKET_EVENTS = {
+  // message
   MESSAGE_NEW: "message:new",
+  MESSAGE_UPDATE: "message:update",
+  MESSAGE_DELETE: "message:update",
 
+  // channel
   CHANNEL_JOIN: "channel:join",
   CHANNEL_LEAVE: "channel:leave",
 };
