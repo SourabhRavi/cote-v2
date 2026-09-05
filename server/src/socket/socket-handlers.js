34fadeaf679bd9f42cf2805585ebe371c16dd927
SourabhRavi <sourabh.k.ravi@gmail.com>
2026-09-05 19:19:57 +0530

    feat(socket): implement typing start and stop events with user notifications

--- server/src/socket/socket-handlers.js
+++ server/src/socket/socket-handlers.js
@@ -1,4 +1,5 @@
 import { markChannelAsRead } from "../modules/channel/channel.service.js";
+import { db } from "../prisma/db.js";
 import { SOCKET_EVENTS } from "./socket-events.js";
 
 export const registerSocketHandlers = (io, socket) => {
@@ -19,5 +20,47 @@ export const registerSocketHandlers = (io, socket) => {
   // socket leaves a channel
   socket.on(SOCKET_EVENTS.CHANNEL_LEAVE, (channelId) => {
     socket.leave(channelId);
+  });
+
+  // socket starts typing in a channel
+  socket.on(SOCKET_EVENTS.TYPING_START, async (channelId) => {
+    try {
+      const channelMember = await db.orm.public.ChannelMember.where({
+        userId: socket.user.id,
+        channelId: channelId,
+      }).first();
+
+      if (!channelMember) return;
+
+      const user = socket.user;
+
+      socket.to(channelId).emit(SOCKET_EVENTS.TYPING_START, {
+        id: user.id,
+        name: user.name,
+      });
+    } catch (error) {
+      console.error("Fill to handle typing start:", error);
+    }
+  });
+
+  // socket stops typing in a channel
+  socket.on(SOCKET_EVENTS.TYPING_STOP, async (channelId) => {
+    try {
+      const channelMember = await db.orm.public.ChannelMember.where({
+        userId: socket.user.id,
+        channelId: channelId,
+      }).first();
+
+      if (!channelMember) return;
+
+      const user = socket.user;
+
+      socket.to(channelId).emit(SOCKET_EVENTS.TYPING_STOP, {
+        id: user.id,
+        name: user.name,
+      });
+    } catch (error) {
+      console.error("Fill to handle typing stop:", error);
+    }
   });
 };
